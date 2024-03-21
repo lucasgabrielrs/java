943ea017f2e59dc460f58ba11bcf1d63f8999ba1
Lucas Gabriel <123906808+lucasgabrielrs@users.noreply.github.com>
2024-03-21 20:36:50 -0300

    Add files via upload

---
+++ AulaForms1/src/telaLogin.java
@@ -0,0 +1,13 @@
+import javax.swing.*;
+
+public class telaLogin {
+    private JPanel p1;
+    private JPasswordField digiteSuaSenhaPasswordField;
+    private JTextField txtLogin;
+    private JButton ENVIARButton;
+    private JComboBox cidadeComboBox;
+
+    private void createUIComponents() {
+        // TODO: place custom component creation code here
+    }
+}
---
+++ AulaForms1/src/telaPrincipal.java
@@ -0,0 +1,5 @@
+import javax.swing.*;
+
+public class telaPrincipal {
+    private JPanel panel1;
+}
